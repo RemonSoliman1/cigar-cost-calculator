e47040426cf8cb788679b9b85fb919642684980e
RemonSoliman <remonsabry44@gmail.com>
2026-09-08 23:38:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='cigar-calc-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>{if(e.request.url.includes('openrate.live'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
